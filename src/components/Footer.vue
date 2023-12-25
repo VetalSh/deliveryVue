7b1b56dd125f863157fbdde15ada3bb128fab893
vitaliiSh <vitalii.shkar@gmail.com>
2023-12-25 23:42:03 +0200

    start

---
+++ src/components/Footer.vue
@@ -0,0 +1,73 @@
+<template>
+  <footer class="footer">
+    <div class="container footer-box">
+      <router-link to="/" class="header-logo logo">
+        <img src="../assets/img/logo.png" alt="logo" />
+      </router-link>
+      <div class="footer-nav">
+        <ul>
+          <a href="./goods.html">Ресторанам</a>
+          <a href="./goods.html">Курьерам</a>
+          <a href="./goods.html">Пресс-центр</a>
+          <a href="./goods.html">Контакты</a>
+        </ul>
+      </div>
+      <div class="footer-social">
+        <img src="../assets/img/social/insta.png" alt="insta" class="social-image" />
+        <img src="../assets/img/social/facebook.png" alt="FB" class="social-image" />
+        <img src="../assets/img/social/vk.png" alt="VK" class="social-image" />
+      </div>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.footer {
+  display: flex;
+  align-items: center;
+  min-height: 156px;
+  padding: 60px 0;
+}
+
+.footer-box {
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  gap: 36px;
+  flex-wrap: wrap;
+}
+
+.footer-nav {
+  flex-grow: 1;
+}
+
+.footer-nav ul {
+  display: flex;
+  align-items: center;
+  list-style: none;
+  padding: 0;
+  margin: 0;
+  gap: 15px;
+  flex-wrap: wrap;
+}
+
+.footer-nav ul a {
+  color: #595959;
+  font-size: 18px;
+  font-weight: 400;
+  text-decoration: none;
+  cursor: pointer;
+}
+
+.footer-social {
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  gap: 20px;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,65 @@
+<template>
+  <section class="banner">
+    <div class="container">
+      <div class="banner-item">
+        <h2 class="banner-title">Онлайн-сервис <br />доставки еды на дом</h2>
+        <p class="banner-text">
+          Блюда из любимого ресторана привезет курьер в перчатках, маске и с
+          антисептиком
+        </p>
+      </div>
+    </div>
+  </section>
+
+  <section class="products">
+    <div class="container">
+      <div class="products-header">
+        <h3 class="products-header--title">Рестораны</h3>
+        <input
+          type="text"
+          class="products-header--search"
+          placeholder="Поиск блюд и ресторанов"
+        />
+      </div>
+
+      <div class="products-wrapper" id="rests-container">
+
+        <router-link
+          class="products-card"
+          v-for="rest in restArray"
+          :key="rest.id"
+          :to="`/rest?id=${rest.id}`"
+        >
+          <div class="products-card__image">
+            <img :src="getUrl(rest.image)" alt="rest-1" />
+          </div>
+          <div class="products-card__description">
+            <div class="products-card__description-row">
+              <h4 class="products-card__description--title">{{ rest.title }}</h4>
+              <div class="products-card__description--badge">{{ rest.time }} мин</div>
+            </div>
+            <div class="products-card__description-info">
+              <div class="products-card__description-info--raiting">
+                <img src="../assets/img/icons/star.svg" alt="star" />
+                {{ rest.raiting }}
+              </div>
+              <div class="products-card__description-info--price">
+                От {{ rest.price }} грн.
+              </div>
+              <div class="products-card__description-info--group">{{ rest.type }}</div>
+            </div>
+          </div>
+        </router-link>
+
+      </div>
+    </div>
+  </section>
+</template>
+
+<script setup>
+import {restArray} from '../constants/rest'
+
+const getUrl = (name) => {
+  return new URL( `../assets/img/${name}`, import.meta.url)
+}
+</script>
